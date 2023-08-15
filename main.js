8893ca6d419757b86fc67ebf595822e95be3c780
Abdul Khaliq <nikotin100k@gmail.com>
2023-08-15 15:34:40 +0500

    Open-Close-Animation

---
+++ main.js
@@ -0,0 +1,39 @@
+let secret = document.querySelector(".secret"); 
+let btn = document.querySelector(".btn");
+let yes = document.querySelector(".yes");
+let no = document.querySelector(".no");
+let box = document.querySelector(".box"); 
+
+function remove(){
+    secret.classList.remove('secret-show' , 'secret-hide'); 
+}
+
+btn.addEventListener('click' , function(){
+    if(secret.classList.contains("secret-show")){
+        secret.classList.add("secret-hide")
+        secret.addEventListener('animationend' , remove);
+    } else {
+        secret.classList.add("secret-show")
+        secret.removeEventListener('animationend' , remove);
+    }
+})
+
+no.addEventListener('click' , function(){
+    if(secret.classList.contains("secret-hide")){
+        secret.classList.add("secret-show")
+        secret.removeEventListener('animationend' , remove);
+    } else {
+        secret.classList.add("secret-hide")
+        secret.addEventListener('animationend' , remove);
+    }
+})
+
+yes.addEventListener('click' , function(){
+    if(secret.classList.contains("secret-hide")){
+        secret.classList.add("secret-show")
+        secret.removeEventListener('animationend' , remove);
+    } else {
+        secret.classList.add("secret-hide")
+        secret.addEventListener('animationend' , remove);
+    }
+})
